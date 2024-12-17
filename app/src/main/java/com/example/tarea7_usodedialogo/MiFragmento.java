64bae40b6775c2032303db959f1582be885c7640
DAMDU212 <samuel.gallego15@gmail.com>
2024-12-17 18:52:47 +0100

    Primer push

--- app/src/main/java/com/example/tarea7_usodedialogo/MiFragmento.java
+++ app/src/main/java/com/example/tarea7_usodedialogo/MiFragmento.java
@@ -1,64 +1,48 @@
 package com.example.tarea7_usodedialogo;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MiFragmento#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MiFragmento extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MiFragmento() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MiFragmento.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MiFragmento newInstance(String param1, String param2) {
-        MiFragmento fragment = new MiFragmento();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+public class MiFragmento extends DialogFragment {
+    @NonNull
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        // Construir el diálogo
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Inflar el layout del diálogo con el xml que hemos creado antes
+        LayoutInflater layoutInflater = requireActivity().getLayoutInflater();
+        builder.setView(layoutInflater.inflate(R.layout.fragment_mi_fragmento, null));
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mi_fragmento, container, false);
+        // Establece el título
+        builder.setTitle("Mi diálogo");
+
+        // Añadir botones de aceptar y cancelar
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Ejecutar al pulsar el botón aceptar
+                    }
+                })
+                .setNegativeButton("KJDSALJLASJLAJS", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Ejecutar al pulsar el botón aceptar
+                        MiFragmento.this.getDialog().cancel();
+                    }
+                });
+
+        // Devuelve el diálogo
+        return builder.create();
+
     }
 }
