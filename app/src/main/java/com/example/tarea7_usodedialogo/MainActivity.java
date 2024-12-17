e296c4d979ebd796ade376d9e6fc4351cc4acc4c
DAMDU212 <samuel.gallego15@gmail.com>
2024-12-17 18:34:25 +0100

    Primer push

--- app/src/main/java/com/example/tarea7_usodedialogo/MainActivity.java
+++ app/src/main/java/com/example/tarea7_usodedialogo/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.tarea7_usodedialogo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Acción al pulsar el fab
+                Toast.makeText(MainActivity.this, "FAB pulsado", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
